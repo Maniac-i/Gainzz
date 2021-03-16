ead3ee545d7d91af444c9470abfd1cebf4b00585
cjrhansen2 <cjrhansen2@gmail.com>
2021-03-16 16:13:57 -0700

    initial server file

---
+++ gainzz/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.listen(PORT, () => {
+    console.log(`API server is running on port ${PORT}!`);
+});
