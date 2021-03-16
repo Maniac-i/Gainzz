4f4fea90bd614fc305e906b451ee9182dfe9775f
cjrhansen2 <cjrhansen2@gmail.com>
2021-03-16 16:15:40 -0700

    initial server

--- gainzz/server.js
+++ gainzz/server.js
@@ -10,6 +10,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+//can add API routes here before HTML routes
+
+//HTML routes
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
