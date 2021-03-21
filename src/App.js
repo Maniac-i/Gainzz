275d219d65bcb73c4de6df328407212f760b6dc4
Joe Maniaci <joe.maniaci1@gmail.com>
2021-03-21 17:25:38 -0400

    moved the Container component and created SpecificExercise folder and files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar"; 
-
+import Container from './components/Container/Container';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+
+        </Container>
       </header>
     </div>
   );
