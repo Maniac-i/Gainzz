686f9933e4efa47a534d2ba9cb286254d38d7e56
Joe Maniaci <joe.maniaci1@gmail.com>
2021-03-22 17:03:35 -0400

    updated specificExercise container to use useState and useEffect

--- client/src/components/SpecificExercise/Container/Container.js
+++ client/src/components/SpecificExercise/Container/Container.js
@@ -1,42 +1,39 @@
-import React, { Component } from "react";
-import SEcard from '../Card/Card';
-import SEjumbotron from '../Jumbotron/Jumbotron';
-import API from '../../../utils/API';
+import React, { useState, useEffect } from "react";
+import SEcard from "../Card/Card";
+import SEjumbotron from "../Jumbotron/Jumbotron";
+import API from "../../../utils/API";
 
+function Container() {
+  const [exercises, setExercises] = useState([]);
 
-class Container extends Component {
-  
-  
-  state = {
-    specificExercise: [],
-  };
-  
-  componentDidMount() {
-    //remove and replace with user id number once signup/login works
+  useEffect(() => {
+    loadExercises()
+  }, [])
+
+  //remove and replace with user id number once signup/login works
   let id = "605611e09c471acaca66eac6";
-  
-  API.findAllByUserId(id)
-    .then((res) => {
-      console.log(res.data)
-      this.setState({ specificExercise: res.data })
 
-    })
-    .catch(err => console.log(err));
-}
-  
-  render() {
-    return(
-    <div className='container'>
+  function loadExercises() {
+    API.findAllByUserId(id)
+      .then((res) => {
+        console.log(res.data);
+        setExercises(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div className="container">
       <SEjumbotron />
-      {this.state.specificExercise.map((exercise) => (
-      <SEcard 
-      exercisename={exercise.name}
-      exercisetype={exercise.type}
-      userId={exercise.userId} />
+      {exercises.map((exercise) => (
+        <SEcard
+          exercisename={exercise.name}
+          exercisetype={exercise.type}
+          userId={exercise.userId}
+        />
       ))}
     </div>
   );
-}
 }
 
 export default Container;
