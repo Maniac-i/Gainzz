41dea7b688956787c41558001faa9375a4383a3c
Joe Maniaci <joe.maniaci1@gmail.com>
2021-03-23 19:58:35 -0400

    rearranged components and pages

--- client/src/pages/ExerciseDetails/index.js
+++ client/src/pages/ExerciseDetails/index.js
@@ -1,9 +1,13 @@
+import { constant } from "async";
 import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
+var dayjs = require('dayjs');
 
 function Container() {
   
   const [details, setDetails] = useState([]);
+
+  const date = (dayjs(details.date).format('MM/DD/YYYY'));
 
   useEffect(() => {
     loadDetails()
@@ -24,7 +28,7 @@ function Container() {
   return (
     <div className="card text-center mx-auto">
   <div className="card-body bg-dark text-white">
-    <h3>Date: {details.date}</h3>
+    <h3>Date: {date}</h3>
     <p className='card-text'>Sets: {details.sets}</p>
     <p className='card-text'>Sets: {details.reps}</p>
     <p className='card-text'>Sets: {details.weight}</p>
