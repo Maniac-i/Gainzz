752b676cae1d173288d1998ed168e22014667cbe
Joe Maniaci <joe.maniaci1@gmail.com>
2021-03-23 09:51:19 -0400

    added exercisdetailsform folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 
 import './App.css';
 import Navbar from "./components/Navbar"; 
-import Container from './components/SpecificExercise/Container/Container';
+import SpecificExerciseContainer from './components/SpecificExercise/Container/Container';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
-        <Container>
+        <SpecificExerciseContainer/>
 
-        </Container>
+        
       </header>
     </div>
   );
