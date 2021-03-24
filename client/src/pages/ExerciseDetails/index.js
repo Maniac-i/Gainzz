f99aee633fd3d5d95a2978127807c1144205afa9
Joe Maniaci <joe.maniaci1@gmail.com>
2021-03-23 21:07:04 -0400

    updated exercisedetails page

--- client/src/pages/ExerciseDetails/index.js
+++ client/src/pages/ExerciseDetails/index.js
@@ -5,16 +5,16 @@ var dayjs = require('dayjs');
 
 function Container() {
   
-  const [details, setDetails] = useState([]);
+  const [detail, setDetail] = useState([]);
   const [allDetails, setAllDetails] = useState([]);
 
-  const date = (dayjs(details.date).format('MM/DD/YYYY'));
+  const date = (dayjs(detail.date).format('MM/DD/YYYY'));
 
   useEffect(() => {
-    loadDetails()
     findAllDetails()
   }, [])
 
+  //find an exercise by id and populates its details.
   function findAllDetails() {
 
     let id = "60563480dd6d07d21c959117"
@@ -22,19 +22,14 @@ function Container() {
     API.populateExerciseDetails(id)
       .then((res) => {
         console.log(res.data)
+        let deets = res.data[0].exerciseDetails;
+        let mostRecentDeet = deets[deets.length - 1];
+
+        setDetail(mostRecentDeet);
+        setAllDetails(deets);
+        console.log(mostRecentDeet);
+        console.log(deets);
       })
-  }
-
-  function loadDetails() {
-
-  let id = '605666e362ed25d679b0163c'
-
-    API.findOne(id)
-      .then((res) => {
-        console.log(res.data)
-        setDetails(res.data);
-      })
-      .catch((err) => console.log(err));
   }
 
   return (
@@ -43,9 +38,9 @@ function Container() {
     <div className="card text-center mx-auto">
   <div className="card-body bg-dark text-white">
     <h3>Date: {date}</h3>
-    <p className='card-text'>Sets: {details.sets}</p>
-    <p className='card-text'>Sets: {details.reps}</p>
-    <p className='card-text'>Sets: {details.weight}</p>
+    <p className='card-text'>Sets: {detail.sets}</p>
+    <p className='card-text'>Sets: {detail.reps}</p>
+    <p className='card-text'>Sets: {detail.weight}</p>
   </div>
 </div>
 </div>
