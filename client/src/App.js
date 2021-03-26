87e34974be6bff40944e9b44cb0a1314a4e3a6ed
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-03-26 19:54:36 -0400

    hompage made

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import SignUp from "./pages/Signup/Signup";
 import SignIn from "./pages/SignIn/Signin";
-import AllExercises from './pages/AllExercises/index';
+import Homepage from './pages/Homepage/index';
 import './App.css';
 import { useAuthTokenStore } from "./utils/auth";
 
@@ -16,7 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={SignUp}/>
           <Route exact path="/signin" component={SignIn}/>
-          <Route exact path='/home' component={AllExercises} />
+          <Route exact path='/home' component={Homepage} />
         </Switch>
       </div>
   </Router>
