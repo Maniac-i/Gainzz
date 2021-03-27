70c9b2a4cbedb4b81e47e15d9ba793c40307fa25
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-03-27 11:48:34 -0400

    execersies page updated and google font added

--- client/src/components/ExerciseCard/index.js
+++ client/src/components/ExerciseCard/index.js
@@ -4,11 +4,11 @@ import React from 'react';
 function SEbutton(props) {
   return(
     
-<div className="card text-center mx-auto">
-  <div className="card-body bg-dark text-white">
+<div className=" exercise card text-center mx-auto bg-transparent ">
+  <div className="card-body text-white border-0">
     <h3>{props.exercisename}</h3>
     <p className='card-text'>Body Part: {props.exercisetype}</p>
-    <div className="btn btn-light">View</div>
+    <div className="btn btn-dark">View</div>
   </div>
 </div>
 
