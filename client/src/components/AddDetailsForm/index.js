6329de51cbc9f84600149de887d8e4bdeca33c88
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-04-04 01:27:00 -0400

    detail form cleaned up

--- client/src/components/AddDetailsForm/index.js
+++ client/src/components/AddDetailsForm/index.js
@@ -36,34 +36,32 @@ function AddDetailsForm(props) {
 
   return(
 
-<div className="add-details card-signup flex-row my-5">
-                        <div className="card-body" >
-                        <form className="form-signin">
-                            <div className="form-label-group">
-                            <input type="text" ref={setRef} id="inputUserame" className="form-control" placeholder="Sets" required autoFocus/>
-                            <label htmlFor="inputUserame"></label>
-                            </div>
+    <div className="add-details bg-light ">
+      <div className="card-body d-flex justify-content-center" >
+        <form className="form-signin">
+          <div className="form-label-group">
+            <input type="number" ref={setRef} id="sets" className="form-control" placeholder="Sets" required autoFocus/>
+            <label htmlFor="sets"></label>
+          </div>
             
-                            <div className="form-label-group">
-                            <input type="text" ref={repRef} id="inputEmail" className="form-control" placeholder="Reps" />
-                            <label htmlFor="inputEmail"></label>
-                            </div>
-                            
-                            <hr/>
+          <div className="form-label-group">
+            <input type="number" ref={repRef} id="reps" className="form-control" placeholder="Reps" />
+            <label htmlFor="reps"></label>
+          </div>
             
-                            <div className="form-label-group">
-                            <input type="text" ref={weightRef} id="inputPassword" className="form-control" placeholder="Weight" required/>
-                            <label htmlFor="inputPassword"></label>
-                            </div>
+          <div className="form-label-group">
+            <input type="text" ref={weightRef} id="weights" className="form-control" placeholder="Weight" required/>
+            <label htmlFor="weights"></label>
+          </div>
             
-                            <div className="button-box col-lg-12">
-                            <button className="btn btn-outline-warning text-uppercase text-center text-dark " 
-                            type="submit" onClick={handleSubmit}>Add Detail</button>
-                            <button className=" btn btn-outline-danger " onClick={props.setAddDetail}>Cancel</button>
-                            </div>
-                        </form>
-                        </div>
-                    </div>
+          <div className="button-box col-lg-12">
+            <button className="btn btn-warning text-uppercase text-center text-dark " 
+              type="submit" onClick={handleSubmit}>Add Detail</button>
+              <button className=" btn btn-danger " onClick={props.setAddDetail}>Cancel</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
